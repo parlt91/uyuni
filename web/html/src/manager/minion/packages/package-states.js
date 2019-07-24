5a476059afcba732ba827e1954b414b368ddafeb
SchoolGuy <egotthold@suse.de>
2019-07-24 10:42:21 +0200

    Pull Search Box to the left and the other two buttons to the right

--- web/html/src/manager/minion/packages/package-states.js
+++ web/html/src/manager/minion/packages/package-states.js
@@ -10,6 +10,7 @@ const Fields = require("components/fields");
 const Messages = require("components/messages").Messages;
 const MessagesUtils = require("components/messages").Utils;
 const SpaRenderer  = require("core/spa/spa-renderer").default;
+const {Toggler} = require("components/toggler");
 
 const AsyncButton = Buttons.AsyncButton;
 const TextField = Fields.TextField;
@@ -357,37 +358,45 @@ class PackageStates extends React.Component {
     return (
       <div>
         {messages}
-        <InnerPanel title={t("Package States")} icon="spacewalk-icon-package-add" buttons={buttons} >
-        <div className="row">
-          <span className="col-md-8 pull-right">
-              <span className="input-group">
-                  <TextField id="package-search" value={this.state.filter} placeholder={t("Search package")}
-                             onChange={this.onSearchChange} onPressEnter={this.triggerSearch}/>
-                  <span className="input-group-btn">
-                      <AsyncButton id="search" text={t("Search")} action={this.search}
-                                   ref={(c) => this.searchButton = c}/>
-                      <button id="system"
-                              className={this.state.view == "system" ? "btn btn-success" : "btn btn-default"}
-                              onClick={this.setView("system")}>{t("System")}</button>
-                      <button id="changes"
-                              className={this.state.view == "changes" ? "btn btn-success" : "btn btn-default"}
-                              disabled={this.state.changed.size == 0} onClick={this.setView("changes")}>
-                          {this.state.changed.size > 0 ? this.state.changed.size : t("No")} {t("Changes")}
-                      </button>
+        <InnerPanel title={t("Package States")} icon="spacewalk-icon-package-add" buttons={buttons}>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="input-group">
+              <TextField id="package-search" value={this.state.filter} placeholder={t("Search package")}
+                         onChange={this.onSearchChange} onPressEnter={this.triggerSearch} className="form-control"/>
+              <span className="input-group-btn">
+                    <AsyncButton id="search" text={t("Search")} action={this.search}
+                                 ref={(c) => this.searchButton = c}/>
                   </span>
-              </span>
-          </span>
-          <table className="table table-striped">
-            <thead>
-            <tr>
-              <th>{t("Package Name")}</th>
-              <th>{t("State")}</th>
-            </tr>
-            </thead>
-            {this.tableBody()}
-          </table>
-        </div>
-      </InnerPanel>
+              </div>
+            </div>
+            <div className="col-md-6">
+            </div>
+            <div className="col-md-2">
+              <div className="input-group pull-right">
+                <button id="system"
+                        className={this.state.view == "system" ? "btn btn-success col" : "btn btn-default col"}
+                        onClick={this.setView("system")}>{t("System")}</button>
+                <button id="changes"
+                        className={this.state.view == "changes" ? "btn btn-success col" : "btn btn-default col"}
+                        disabled={this.state.changed.size == 0} onClick={this.setView("changes")}>
+                  {this.state.changed.size > 0 ? this.state.changed.size : t("No")} {t("Changes")}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <table className="table table-striped">
+              <thead>
+              <tr>
+                <th>{t("Package Name")}</th>
+                <th>{t("State")}</th>
+              </tr>
+              </thead>
+              {this.tableBody()}
+            </table>
+          </div>
+        </InnerPanel>
       </div>
     );
   }
