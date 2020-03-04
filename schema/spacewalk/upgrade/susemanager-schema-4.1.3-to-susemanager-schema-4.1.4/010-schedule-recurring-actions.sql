a03d969713539e5257a9e5444a93821589552aaf
Pascal Arlt <parlt@suse.com>
2020-03-04 08:46:45 +0100

    Schema: Migrations + changelog

---
+++ schema/spacewalk/upgrade/susemanager-schema-4.1.3-to-susemanager-schema-4.1.4/010-schedule-recurring-actions.sql
@@ -0,0 +1,59 @@
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+SELECT sequence_nextval('rhn_tasko_bunch_id_seq'), 'recurring-state-apply-bunch', 'Applies salt state to minion/group/org', null
+WHERE NOT EXISTS ( SELECT 1 FROM rhnTaskoBunch WHERE name = 'recurring-state-apply-bunch');
+
+INSERT INTO rhnTaskoTask (id, name, class)
+SELECT sequence_nextval('rhn_tasko_task_id_seq'), 'recurring-state-apply', 'com.redhat.rhn.taskomatic.task.RecurringStateApplyJob'
+WHERE NOT EXISTS ( SELECT 1 FROM rhnTaskoTask WHERE name = 'recurring-state-apply');
+
+INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
+SELECT sequence_nextval('rhn_tasko_template_id_seq'), ( SELECT id FROM rhnTaskoBunch WHERE name = 'recurring-state-apply-bunch' ), ( SELECT id FROM rhnTaskoTask WHERE name = 'recurring-state-apply' ), 0, null
+WHERE NOT EXISTS ( SELECT 1 FROM rhnTaskoTemplate WHERE bunch_id = ( SELECT id FROM rhnTaskoBunch WHERE name = 'recurring-state-apply-bunch' ) );
+
+CREATE TABLE suseRecurringAction
+(
+  id                NUMERIC NOT NULL
+                    CONSTRAINT suse_recurring_action_id_pk PRIMARY KEY,
+  target_type       VARCHAR(32) NOT NULL,
+  name              VARCHAR(256) NOT NULL,
+  cron_expr         VARCHAR(32) NOT NULL,
+  minion_id         NUMERIC
+                    CONSTRAINT suse_rec_action_minion_fk
+                      REFERENCES suseMinionInfo(server_id)
+                      ON DELETE CASCADE,
+  group_id          NUMERIC
+                    CONSTRAINT suse_rec_action_group_fk
+                      REFERENCES rhnServerGroup(id)
+                      ON DELETE CASCADE,
+  org_id            NUMERIC
+                    CONSTRAINT suse_rec_action_org_fk
+                      REFERENCES web_customer(id)
+                      ON DELETE CASCADE,
+  creator_id        NUMERIC
+                    CONSTRAINT suse_rec_action_creator_fk
+                      REFERENCES web_contact(id)
+                      ON DELETE CASCADE,
+  active            CHAR(1) DEFAULT ('Y') NOT NULL,
+  test_mode         CHAR(1) DEFAULT ('Y') NOT NULL,
+  created           TIMESTAMP WITH TIME ZONE
+                      DEFAULT (current_timestamp)
+                      NOT NULL,
+  modified          TIMESTAMP WITH TIME ZONE
+                      DEFAULT (current_timestamp)
+                      NOT NULL
+);
+
+CREATE SEQUENCE suse_recurring_action_id_seq;
+
+CREATE INDEX suse_rec_action_type
+    ON suseRecurringAction(target_type);
+
+CREATE UNIQUE INDEX suse_rec_action_name_minion_uq
+    ON suseRecurringAction(name, minion_id);
+
+CREATE UNIQUE INDEX suse_rec_action_name_grp_uq
+    ON suseRecurringAction(name, group_id);
+
+CREATE UNIQUE INDEX suse_rec_action_name_org_uq
+    ON suseRecurringAction(name, org_id);
+
